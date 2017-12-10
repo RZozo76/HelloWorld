442d0b4cf7035a5c53d7f736d9bfee9770852105
RZozo76 <rossmannz@gmail.com>
2017-12-10 23:16:11 +0100

    #5 HelloWorld

---
+++ src/hu/helixlab/Main.java
@@ -0,0 +1,9 @@
+package hu.helixlab;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("************\n* Hello \t*\n* World \t*\n* in    \t*\n* a     \t*\n* frame \t*\n************");
+    }
+}
